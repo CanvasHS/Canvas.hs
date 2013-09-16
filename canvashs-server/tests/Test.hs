1eedb0577d90b27ce2af37077f52b0155915a809
Joost van Doorn <joost.van.doorn@gmail.com>
2013-09-16 14:54:11 +0200

    Basic cabal build script and test script

---
+++ canvashs-server/tests/Test.hs
@@ -0,0 +1,9 @@
+import Data.Char
+import Data.List
+import Test.QuickCheck
+import Text.Printf
+ 
+main  = mapM_ (\(s,a) -> printf "%-25s: " s >> a) tests
+ 
+-- ("testname", quickCheck function)
+tests  = []
