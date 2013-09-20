c5db3a7a6dc90d85b127b34054a0bf6fd8ed5577
Travis <travis@travis-ci.org>
2013-09-20 12:51:44 +0200

    Github pages, verbeteren van het pushToGit script, laten appenden aan een html file

--- pushToGithub.sh
+++ pushToGithub.sh
@@ -1,16 +1,18 @@
 echo -e "Pushing build to github-pages"
-
+# move the distributed source to home
 cp dist/canvashs*.tar.gz $HOME/canvashs.tar.gz
-
+# move to home
 cd $HOME
+# lets config git a little
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
-
+# clone the pages repo to gh-pages
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com:CanvasHS/Canvas.hs.git gh-pages > /dev/null
-
-cd gh-pages/dists
-cp $HOME/canvashs*.tar.gz canvashs-${TRAVIS_BUILD_NUMBER}.tar
-
+#change dir to dist
+cd gh-pages/dist
+#copy build to right direction
+cp $HOME/canvashs.tar.gz canvashs-${TRAVIS_BUILD_NUMBER}.tar.gz
+echo "<tr><td> #$TRAVIS_BUILD_NUMBER </td><td> 0.1 </td> <td> `date` </td><td> <a href=\"dist/canvashs-$TRAVIS_BUILD_NUMBER.tar.gz\" $TRAVIS_BUILD_NUMBER.tar.gz </td></tr>" >> ../_includes/list.html
 git add .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
