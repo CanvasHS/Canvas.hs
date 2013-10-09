82a6a5ca8cf7a77e904b035b2f522879055d18ce
Pim Jager <pim@pimjager.nl>
2013-10-09 15:20:26 +0200

    module,protocol: haddock toegevoegd

--- canvashs-module/CanvasHs/Protocol.hs
+++ canvashs-module/CanvasHs/Protocol.hs
@@ -1,12 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+{- |
+	Deze module handelt het omzetten van het interne datamodel naar het protocol af d.m.v. de
+	encode-functie en het omzetten de andere kant op d.m.v. de decode-functie
+	
+	Op dit moment is de module in dummy-staat om de inrichting van de code duidelijk te hebben,
+	de daadwerkelijke invulling (body) van de functies zal nog veranderen.
+-}
 module CanvasHs.Protocol
 (	encode
 ) where 
 
 import qualified Data.Text as T
 import CanvasHs
-
+-- | encode maakt van een Output een JSON-string (type Data.Text.Text) die voldoet aan het protocol
+--   LET OP: het resultaat voldoet momenteel NIET aan het protocol!
 encode :: Output -> T.Text
 encode (Circle p r)		= T.pack $ unlines 
 							["{Shape : 'Cicle'" 
