79dd50e9b70c888871091ee7ee38398e400faafe
Pim Jager <pim@pimjager.nl>
2013-10-14 20:34:12 +0200

    CanvasHs, Server, Protocol: Het is nu daadwerkelijke mogelijk en EventHandler toe te voegen

--- canvashs-module/CanvasHs.hs
+++ canvashs-module/CanvasHs.hs
@@ -6,12 +6,15 @@
 -}
 module CanvasHs
 (	
-	installEventHandler, basicTest
+	installEventHandler
 )  where
 
 import CanvasHs.Data
 import CanvasHs.Server
 import CanvasHs.Launch
+import CanvasHs.Protocol
+
+import qualified Data.Text as T
 
 import Data.IORef (IORef, newIORef, atomicWriteIORef, readIORef)
 import Control.Monad.Trans (liftIO, lift)
@@ -28,14 +31,18 @@ installEventHandler ::
 	-> 	IO ()	
 installEventHandler handl startState = do
 	store <- newIORef (State{extState=startState, callback=handl})
-	-- server starten, dingen openen, store rondgooien, jwz
-	return ()
-
--- This is a basic server session without the state handler
-basicTest :: IO ()
-basicTest = do
 	launchBrowser "http://localhost:8000"
-	start -- starts server
+	start $ handleInput store
+	return ()
+	
+-- | handles input from the canvas, calls the handler on it and sends the result back to the canvas
+handleInput :: IORef (State a) -> T.Text -> IO (Maybe T.Text)
+handleInput st ip	= do
+							curState <- readState st
+							let
+								(newState, shapes) = (callback curState) (extState curState) $ decode ip
+							updateState st curState{extState=newState}
+							return $ Just $ encode $ head shapes  -- Just $ encode shapes
 	
 -- | updates the stored State to the new State and returns the new State
 updateState :: IORef (State a) -> (State a) -> IO (State a)
