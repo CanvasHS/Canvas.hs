db78e7365a1fdccab89b4361bc42859d3471957e
windwarrior <lennartbuit@gmail.com>
2013-10-25 17:44:57 +0200

    Cabal: js tests reenabled

--- canvashs-module/tests/CanvasHs/Protocol/InputSpec.hs
+++ canvashs-module/tests/CanvasHs/Protocol/InputSpec.hs
@@ -1,13 +1,16 @@
 module CanvasHs.Protocol.InputSpec (main,spec) where
 
 import Test.Hspec
+import Test.QuickCheck
 import CanvasHs.Protocol.Input
 import CanvasHs.Data
 import CanvasHs.Protocol (decode)
-
+import Data.Char
 import Data.Maybe
-
-import Data.Text (pack)
+import Data.Text
+ 
+instance Arbitrary Char where
+    arbitrary     = choose ('\32', '\128')
 
 main :: IO ()
 main = hspec spec
@@ -19,6 +22,10 @@ spec = do
                 (decode $ pack $ "{\"event\":\"mousedown\", \"eventData\":{\"id\": \"myAwesomeShape\", \"x\": 150, \"y\":200}}") `shouldBe` (MouseDown (150, 200) "myAwesomeShape")
             it "can decode an mouseclick event" $ do
                 (decode $ pack $ "{\"event\":\"mouseclick\", \"eventData\":{\"id\": \"myAwesomeShape\", \"x\": 150, \"y\":200}}") `shouldBe` (MouseClick (150, 200) "myAwesomeShape")
+
+
+--            it "returns an *arbitrary* mousedown event" $
+--                property $ \x y eid -> (decode $ buildMouse "mousedown" x y eid) == (MouseDown (x, y) eid)
             it "can decode an mouseup event" $ do
                 (decode $ pack $ "{\"event\":\"mouseup\", \"eventData\":{\"id\": \"myAwesomeShape\", \"x\": 150, \"y\":200}}") `shouldBe` (MouseUp (150, 200) "myAwesomeShape")
             it "can decode an mouseenter event" $ do
@@ -33,3 +40,7 @@ spec = do
                 (decode $ pack $ "{\"event\":\"keyup\", \"eventData\":{\"key\": \"c\", \"control\": true, \"alt\": true, \"shift\": true, \"super\": true}}") `shouldBe` (KeyUp 'c' [Ctrl, Alt, Shift, Super])
             it "can decode a scroll event" $ do
                 (decode $ pack $ "{\"event\":\"scroll\", \"eventData\":{\"xdelta\": 10, \"ydelta\": 10}}") `shouldBe` (Scroll 10 10)
+
+
+buildMouse :: String -> Int -> Int -> String -> Text
+buildMouse ty x y eid = pack $ "{\"event\":\"" ++ ty ++ "\", \"eventData\":{\"id\": \"" ++ (eid) ++ "\", \"x\": " ++ (show x) ++ ", \"y\": " ++ (show y) ++ "}}"
