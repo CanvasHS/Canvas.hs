62fe6b6056c135a350fc53e61c35587a80c779b3
windwarrior <lennartbuit@gmail.com>
2013-11-08 02:20:05 +0100

    Demo: now with the files i have forgotten

---
+++ demo/Buttons.hs
@@ -0,0 +1,7 @@
+module Buttons where
+
+import CanvasHs.Data
+
+-- button = Container 32 32 [ Fill (255, 255, 255,1.0) $ Polygon [(0, 0), (32,0), (32,28), (28, 32), (5, 32), (0, 28)], Polygon [(4,16), (16,4), (28,16), (24,20), (16,12), (8,20)]]
+
+button = Fill (255,255,255,0.75) $ Rect (1,1) 31 31
