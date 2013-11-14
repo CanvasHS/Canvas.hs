1947653a9778ce5c270541102c866806a178cc92
Travis <travis@travis-ci.org>
2013-11-14 21:13:14 +0100

    Protocol: Added text alignment, doesn't work yet, but thats something related to javascript

--- gistfile1.hs
+++ gistfile1.hs
@@ -8,7 +8,14 @@ main = installEventHandler handl 1
 
 -- we gebruiken een simpele Int als store (maar het zou ook een record oid kunnen zijn)
 handl :: Int -> Event -> (Int, Shape)
-handl st StartEvent  = (st, Container 20 10 [(Event defaults{eventId="123",mouseClick=True} (Circle (34, 34) 34))
-	])    -- Eerste image tekenen
+--handl st StartEvent  = (st, Container 20 10 [(Event defaults{eventId="123",mouseClick=True} (Circle (34, 34) 34))
+--	])    -- Eerste image tekenen
+handl st StartEvent = (st, 
+    Container 900 600 [
+        (Text (100,0) "Hello World" defaults{alignment=Start}),
+        (Text (100,10) "Hello World" defaults{alignment=Center}),
+        (Text (100,20) "Hello World" defaults{alignment=End})
+    ])
+
 handl st (MouseClick (x,y) "123") = (st, Container 20 10 [(Event defaults{eventId="123",mouseClick=True} (Circle (x, y) 34))])    -- Eerste image tekenen
 handl st _      = (st+1, (Circle (1, 1) st))  -- bij elk volgende event tekenen we er een klein cirkeltje bij
