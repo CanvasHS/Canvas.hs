3e5024de5a2d76ba56db77b0e4cf367411f433bb
Pim Jager <pim@pimjager.nl>
2013-11-25 16:59:29 +0100

    Protocol: switcht to local and remote output

--- canvashs-module/CanvasHs.hs
+++ canvashs-module/CanvasHs.hs
@@ -1,12 +1,12 @@
 {- |
-	De module CanvasHs maakt het mogelijk om op een eenvoudige manier te tekenen op een HTML5-canvaselement
-	
-	Op dit moment is de module in dummy-staat om de inrichting van de code duidelijk te hebben,
-	de daadwerkelijke invulling (body) van de functies zal nog veranderen.
+    De module CanvasHs maakt het mogelijk om op een eenvoudige manier te tekenen op een HTML5-canvaselement
+    
+    Op dit moment is de module in dummy-staat om de inrichting van de code duidelijk te hebben,
+    de daadwerkelijke invulling (body) van de functies zal nog veranderen.
 -}
 module CanvasHs
-(	
-	installEventHandler
+(
+    installEventHandler
 )  where
 
 import CanvasHs.Data
@@ -23,30 +23,32 @@ import Control.Monad.Trans (liftIO, lift)
 type Callback a = (a -> Event -> (a, Output))
 
 -- | Our internal state, holds the user state and a refrence tot he user handler
-data State a = 	State 	{extState :: a
-						,callback :: Callback a
-						}
+data State a =  State   {extState :: a
+                        ,callback :: Callback a
+                        }
 
                       
                         
 -- | Start CanvasHs om grafische weergave mogelijk te maken. registreert de event handler en de start state van
 --	 de de user.
 installEventHandler :: 
-		Callback userState -- ^ event handler on current state and incoming event, that produces a tuple of the new user state and ouput to process
-	->	userState -- ^ start state
-	-> 	IO ()	
+        Callback userState -- ^ event handler on current state and incoming event, that produces a tuple of the new user state and ouput to process
+    ->  userState -- ^ start state
+    ->  IO ()
 installEventHandler handl startState = do
-	store <- newIORef (State{extState=startState, callback=handl})
-	launchBrowser "http://localhost:8000"
-	start $ handleInput store
-	return ()
-	
+    store <- newIORef (State{extState=startState, callback=handl})
+    launchBrowser "http://localhost:8000"
+    start $ handleInput store
+    return ()
+    
 -- | handles input from the canvas, calls the handler on it and sends the result back to the canvas
 handleInput :: IORef (State a) -> T.Text -> IO (Maybe T.Text)
 handleInput st ip	= do
-							curState <- readIORef st
-							let
-								(newState, output) = (callback curState) (extState curState) $ decode ip
-							atomicModifyIORef st (\_ -> (curState{extState=newState}, ())) --update de state
-							return $ Just $ encode output
+                            curState <- readIORef st
+                            let
+                                (newState, output) = (callback curState) (extState curState) $ decode ip
+                            atomicModifyIORef st (\_ -> (curState{extState=newState}, ())) --update de state
+                            case output of 
+                                   (R o)   -> return $ Just $ encode o
+                                   (HS o)  -> return Nothing
 
