558ddc285b20a063fd31eb59f9b0258f95221240
Pim Jager <pim@pimjager.nl>
2013-12-02 16:21:25 +0100

    Protocol: renamed iEncode to shapeEncode

--- canvashs-module/CanvasHs/Protocol.hs
+++ canvashs-module/CanvasHs/Protocol.hs
@@ -1,19 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 {- |
-	Deze module handelt het omzetten van het interne datamodel naar het protocol af d.m.v. de
-	encode-functie en het omzetten de andere kant op d.m.v. de decode-functie
-	
-	Op dit moment is de module in dummy-staat om de inrichting van de code duidelijk te hebben,
-	de daadwerkelijke invulling (body) van de functies zal nog veranderen.
+    Deze module handelt het omzetten van het interne datamodel naar het protocol af d.m.v. de
+    encode-functie en het omzetten de andere kant op d.m.v. de decode-functie
+    
+    Op dit moment is de module in dummy-staat om de inrichting van de code duidelijk te hebben,
+    de daadwerkelijke invulling (body) van de functies zal nog veranderen.
 -}
 module CanvasHs.Protocol
-(	encode
-	,decode
+(   encode
+    ,decode
 ) where 
 
 import CanvasHs.Data
-import CanvasHs.Protocol.Output
+import CanvasHs.Protocol.ShapeOutput
 import CanvasHs.Protocol.Input
 
 import qualified Data.Text as T
@@ -23,12 +23,12 @@ import qualified Data.ByteString.Lazy.UTF8 as BU (fromString, toString)
 import Data.Either (either)
 import Data.Maybe (fromMaybe)
 
-{- 	|
-	encode maakt van een Output een JSON-string (type Data.Text) die voldoet aan het protocol
-	@ensure \result is een valide JSON-object
+{-  |
+    encode maakt van een Output een JSON-string (type Data.Text) die voldoet aan het protocol
+    @ensure \result is een valide JSON-object
 -}
 encode :: RemoteOutput -> T.Text
-encode = T.pack . BU.toString . Aeson.encode . iEncode . (fromMaybe (Container 900 600 [])) . fst -- ^TODO: actually handle Output
+encode = T.pack . BU.toString . Aeson.encode . shapeEncode . (fromMaybe (Container 900 600 [])) . fst -- ^TODO: actually handle Output
 
 -- | Ontsleuteld een inkomend bericht naar een event
 --   De daadwerkelijke code hiervoor staat in CanvasHs.Protocol.Output
