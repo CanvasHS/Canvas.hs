18836c9b939e4142488d522696c2a67cd201827c
Joost van Doorn <joost.van.doorn@gmail.com>
2014-01-23 14:44:40 +0100

    Fixed git version

--- tests/SetupTests.hs
+++ tests/SetupTests.hs
@@ -6,7 +6,7 @@ main = cloneRepos
 
 cloneRepos
   = do putStrLn "Install Test scripts for Javascript "
-       putStrLn "Git is required http://git-scm.com/ (make sure it is in the PATH) "
+       putStrLn "Git is required http://git-scm.com/ (make sure it is in the PATH) version >= 1.7.10"
        executeShellCommand "git --version"
        executeShellCommand "git clone https://github.com/pivotal/jasmine.git lib/jasmine -b v1.3.1"
        executeShellCommand "git clone https://github.com/HumbleSoftware/js-imagediff.git lib/js-imagediff -b v1.0.4"
